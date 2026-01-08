20be5234800256eadca1d386a8724622bf05c406
Yi888a <Ziyil8@nycstudents.net>
2026-01-08 09:26:21 -0500

    cahnged

--- finalprojecttest.py
+++ finalprojecttest.py
@@ -1,6 +1,6 @@
 import random
 
-class Player:
+class player:
     def __init__(self,name,ovr,pockets,attributes,fame,morale,energy,train,game,quit):
         self.name = name
         self.ovr = int(0)
@@ -13,36 +13,43 @@ class Player:
         self.game = game
         self.quit = quit
 
-class NPC:
-    def __init__(self, name,):
-      
-
-name = input ("Welcome to the Career what would you like to name your player" )
-my_player = Player(f"{name}")
-print("Slums, you are just a kid in the Slums that had hoop dreams, until Georgie came to you and asked if you really wanted to get out and pursue the high life.")
-def menu(my_player):
-    play=True
-    while play == True:
-        print("1) Interact with)
-        print("2) Go to the Store")
-        print("3)Play basketball game")
-        print("4)quit")
-        y = input("what would you like to do")
-        if y == "1":
-            input("Georgie: Yo lets go, give me more pushups. (1) Yes Sir. (2) Yo Georgie lets go work on my handles.")
-             if input = 1
-             print ("Your overall has improved by 10 ")
-                    
-
-
-        elif y == "2":
-            Player.energy(my_energy)
-        elif y == "3":
-            Player.game(my_game)
-        elif y == "4":
+class Npc:
+    def __init__(self,name):
+        self.name = name
+        
+def menu():
+     print("Slums, you are just a kid in the Slums that had hoop dreams, until Georgie came to you and asked if you really wanted to get out and pursue the high life.")
+     while True:
+        print("What would you like to do next? (1) View Player Stats (2) Train Player (3) Talk to NPCs (4) Play a game (5) Quit")
+        choice = input("Enter your choice (1-4): ")
+        if choice == '1':
+            display_stats(player)
+        elif choice == '2':
+            train(player)
+        elif choice == '3':
+            interact(player)
+        elif choice == '4':
+            game(player)
+        elif choice == '5':
+            print("Goodbye!")
             break
         else:
-            print("You have to choose 1,2,3, or 4")
-menu(my_player)
+            print("Invalid choice. Try again.")
+   
 
+def game(player):
+    print("Scrim in progress...")
+    if player.ovr >= 85:
+        print(f"{player.name} performed excellently in the scrim!")
+        print(f"Reward: +{random.randint(50,100)}$ to Pockets")
+    elif player.ovr <= 75:
+        print(f"{player.name} got whooped.")
+        train_player(player)
+    else:
+        print(f"Scrim did not go so well...")
+        train_player(player)
+        
+
+if __name__ == "__main__":
+    menu()
 
